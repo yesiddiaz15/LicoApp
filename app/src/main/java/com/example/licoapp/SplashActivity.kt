8be74540aef36d75812dd1769278843a9a1f3dc1
juanfelipegarciagomez <juangarcia@wposs.com>
2022-02-03 14:24:11 -0500

    adjustment

--- app/src/main/java/com/example/licoapp/SplashActivity.kt
+++ app/src/main/java/com/example/licoapp/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.example.licoapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.licoapp.ui.view.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

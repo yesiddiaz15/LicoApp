f1069ca205fdb5a113c0e99afdaba1bf8b34e7e8
juanfelipegarciagomez <juangarcia@wposs.com>
2022-02-03 14:09:18 -0500

    add bottom_navigation and icons

--- app/src/main/java/com/example/licoapp/MainActivity.kt
+++ app/src/main/java/com/example/licoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.licoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TODO make logic bottom_view
     }
 }

705c33ae8b59571d683e8218a2304c529f0181c1
Eduar Diaz <eduardiaz@wposs.com>
2022-02-03 12:38:23 -0500

    Add dependencies

--- app/src/main/java/com/example/licoapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/licoapp/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.licoapp
+package com.example.licoapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.licoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

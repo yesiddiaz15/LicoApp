5fbf2878c775713b98261ab90a3af901644bd939
Eduar Diaz <eduardiaz@wposs.com>
2022-02-07 14:41:49 -0500

    Correct view binding and assign that after the splash it goes to login

--- app/src/main/java/com/example/licoapp/SplashActivity.kt
+++ app/src/main/java/com/example/licoapp/SplashActivity.kt
@@ -1,14 +1,16 @@
 package com.example.licoapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.licoapp.ui.view.MainActivity
+import com.example.licoapp.ui.view.LoginActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
